ea2c412bfbd8ea6e6cdc5211c4651a95dea35347
lindamood <wesley.lindamood@gmail.com>
2013-12-17 11:52:05 -0500

    setting up the titlecard layout and basic page structure

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,50 @@
-$(function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
-    });
+$(document).ready(function() {
+    var $container = $('#content');
+    var $titlecard = $('.titlecard')
+    var $titlecard_wrapper = $('.titlecard-wrapper');
+    var $w = $(window);
+    
+    var aspect_width = 6;
+    var aspect_height = 4;
+    var window_width;
+    var window_height;
+    
+    function on_resize() {
+        var w;
+        var h;
+        var w_optimal;
+        var h_optimal;
+        var w_offset = 0;
+        var h_offset = 0;
 
-    var html = JST.example(context);
-
-    $('#template-example').html(html);
+        window_width = $w.width();
+        window_height = $w.height();
+        
+        // calculate optimal width if height is constrained to window height
+        w_optimal = (window_height * aspect_width) / aspect_height;
+        
+        // calculate optimal height if width is constrained to window width
+        h_optimal = (window_width * aspect_height) / aspect_width;
+        
+        // decide whether to go with optimal height or width
+        if (w_optimal > window_width) {
+            w = w_optimal;
+            h = window_height;
+        } else {
+            w = window_width;
+            h = h_optimal;
+        }
+        w_offset = (window_width - w) / 2;
+        h_offset = (window_height - h) / 2;
+        
+        // size the divs accordingly
+        $titlecard.width(w + 'px').height(h + 'px');
+        $titlecard.css('margin', h_offset + 'px ' + w_offset + 'px');
+        $titlecard_wrapper.height(window_height + 'px');
+        $container.css('marginTop', window_height + 'px');
+    }
+    
+    $(window).on('resize', on_resize);
+    on_resize();
+    
 });
