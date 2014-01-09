8f5585fa8eabb731410731a5b5b41d148429bd5d
Tyler Fisher <tyler@tylerjfisher.com>
2014-01-09 09:17:58 -0500

    new lightbox that sort of works

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,8 @@ var $begin;
 var $toggle_ambient;
 var $button_download_audio;
 var $button_toggle_caption;
+var $lightbox;
+var $lightbox_image;
 var ambient_is_paused = false;
 var ambient_start = 0;
 var ambient_end = 53;
@@ -184,6 +186,61 @@ var on_waypoint = function(element, direction) {
 
 };
 
+var lightbox_image = function(element) {
+    $('body').append('<div id="lightbox"></div>')
+
+    $lightbox = $('#lightbox');
+    var $el = $(element);
+
+    // fade in the overlay
+
+    $lightbox.css({
+        display: 'block',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        'z-index': 500,
+        opacity: 1
+    });
+
+    var new_image_src = $el.attr('src');
+
+    $lightbox.append('<img src="' + new_image_src + '" id="lightbox_image">');
+    $lightbox_image = $('#lightbox_image');
+
+    $lightbox_image.css({
+        'max-width': $w.width() - 10 + 'px',
+        'max-height': $w.height() - 10 + 'px',
+        'opacity': 1
+    })
+
+    // disable scrolling
+
+
+    // listen for click
+
+    $lightbox.on('click', remove_lightbox);
+}
+
+var remove_lightbox = function() {
+    console.log('fired');
+    $el = $('#lightbox');
+
+    $el.find('img').css({
+        opacity: 0
+    });
+
+    $el.css({
+        opacity: 0,
+        position: 'relative'
+    });
+
+    $el.remove();
+}
+
+
 $(document).ready(function() {
     $container = $('#content');
     $titlecard = $('.titlecard');
@@ -199,6 +256,7 @@ $(document).ready(function() {
     $toggle_ambient = $( '.toggle-ambi' );
     $button_download_audio = $('#download-audio');
     $button_toggle_caption = $('.caption-label');
+    $overlay = $('#fluidbox-overlay');
 
     // Set up the STORY NARRATION player.
     $player.jPlayer({
@@ -270,6 +328,10 @@ $(document).ready(function() {
         return false;
     });
 
+    $('.img-responsive').on('click', function() {
+        lightbox_image(this);
+    });
+
     $button_download_audio.on('click', function(){
         _gaq.push(['_trackEvent', 'Audio', 'Downloaded story audio mp3', APP_CONFIG.PROJECT_NAME, 1]);
         console.log('Downloaded story audio mp3');
@@ -291,160 +353,6 @@ $(document).ready(function() {
       });
 
     $(window).on('resize', on_resize);
-
-    //fluidbox
-
-    // Global variables
-    var $fb = $('a[data-fluidbox]'),
-      vpRatio;
-
-    // Add class
-    $fb.addClass('fluidbox');
-
-    // Create fluidbox modal background
-    $('body').append('<div id="fluidbox-overlay" />');
-
-    // The following events will force FB to close
-    var closeFb = function (){
-        $('a[data-fluidbox].fluidbox-opened').trigger('click');
-      },
-      positionFb = function ($activeFb){
-        // Get elements
-        var $img = $activeFb.find('img'),
-            $ghost = $activeFb.find('.fluidbox-ghost');
-
-        // Calculate offset and scale
-        var offsetY = $(window).scrollTop()-$img.offset().top+0.5*($img.data('imgHeight')*($img.data('imgScale')-1))+0.5*($(window).height()-$img.data('imgHeight')*$img.data('imgScale')),
-            offsetX = 0.5*($img.data('imgWidth')*($img.data('imgScale')-1))+0.5*($(window).width()-$img.data('imgWidth')*$img.data('imgScale'))-$img.offset().left,
-            scale = $img.data('imgScale');
-
-        // Animate ghost element
-        $ghost.css({
-          'transform': 'translate('+offsetX+'px,'+offsetY+'px) scale('+scale+')'
-        });
-      };
-
-    // Close Fluidbox when overlay is closed
-    $('#fluidbox-overlay').click(closeFb);
-
-    // Check if images are loaded first
-
-    // Create dynamic elements
-    $fb
-    .wrapInner('<div class="fluidbox-wrap" />')
-    .find('img')
-      .css({ opacity: 1 })
-      .after('<div class="fluidbox-ghost" />');
-
-    // Listen to resize event for calculations
-    $(window).resize(function (){
-
-      // Get viewport ratio
-      vpRatio = $(window).width() / $(window).height();
-
-      // Get dimensions and aspect ratios
-      $fb.each(function (){
-        var $img   = $(this).find('img'),
-            $ghost = $(this).find('.fluidbox-ghost'),
-            $wrap  = $(this).find('.fluidbox-wrap'),
-            data   = $img.data();
-
-        // Save image dimensions as jQuery object
-        data.imgWidth  = $img.width();
-        data.imgHeight = $img.height();
-        data.imgRatio  = $img.width() / $img.height();
-
-        // Resize and position ghost element
-        $ghost.css({
-          width: $img.width(),
-          height: $img.height(),
-          top: $img.offset().top - $wrap.offset().top,
-          left: $img.offset().left - $wrap.offset().left,
-        });
-
-        // Calculate scale based on orientation
-        if(vpRatio > data.imgRatio) {
-          data.imgScale = $(window).height()*0.95 / $img.height();
-        } else {
-          data.imgScale = $(window).width()*0.95 / $img.width();
-        }
-
-      });
-
-      // Reposition Fluidbox, but only when one is found to be opened
-      var $activeFb = $('a[data-fluidbox].fluidbox-opened');
-      if($activeFb.length > 0) positionFb($activeFb);
-
-    }).resize();
-
-    // Bind click event
-    $fb.click(function (e){
-      // Variables
-      var $activeFb = $(this),
-          $img   = $(this).find('img'),
-          $ghost = $(this).find('.fluidbox-ghost');
-
-      _gaq.push(['_trackEvent', 'Photo', 'Zoomed on ' + $img.attr('src'), APP_CONFIG.PROJECT_NAME, 1]);
-
-      if($(this).data('fluidbox-state') === 0 || !$(this).data('fluidbox-state')) {
-        // State: Closed
-        // Action: Open fluidbox
-
-        // Switch state
-        $(this)
-        .data('fluidbox-state', 1)
-        .removeClass('fluidbox-closed')
-        .addClass('fluidbox-opened');
-
-        // Show overlay
-        $('#fluidbox-overlay').fadeIn();
-
-        // Set thumbnail image source as background image first, preload later
-        $ghost.css({
-          'background-image': 'url('+$img.attr('src')+')',
-          opacity: 1
-        });
-
-        // Hide original image
-        $img.css({ opacity: 0 });
-
-        // Preload ghost image
-        var ghostImg = new Image();
-        ghostImg.onload = function (){
-          $ghost.css({ 'background-image': 'url('+$activeFb.attr('href')+')' });
-        };
-        ghostImg.src = $(this).attr('href');
-
-        // Position Fluidbox
-        positionFb($(this));
-
-      } else {
-        // State: Open
-        // Action: Close fluidbox
-
-        // Switch state
-        $(this)
-        .data('fluidbox-state', 0)
-        .removeClass('fluidbox-opened')
-        .addClass('fluidbox-closed');
-
-        // Hide overlay
-        $('#fluidbox-overlay').fadeOut(250);
-
-        // Show original image
-        $img.css({ opacity: 1 });
-
-        // Reverse animation on wrapped elements
-        $ghost
-        .css({ 'transform': 'translate(0,0) scale(1)' })
-        // .one('webkitTransitionEnd MSTransitionEnd oTransitionEnd otransitionend transitionend', function (){
-        //   // Wait for transntion to complete before hiding the ghost element
-        //   $ghost.css({ opacity: 0 });
-        // });
-
-      }
-      e.preventDefault();
-    });
 
     on_resize();
     sub_responsive_images();
